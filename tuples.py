aab333c55b908b09d123ba494b48ab4a02dd68bf
Luca T <luca.terraz@gmail.com>
2018-04-01 16:31:07 +0200

    Added tuples examples

---
+++ tuples.py
@@ -0,0 +1,9 @@
+# tuples examples
+# similar to lists, but immutable
+
+my_tuple = (1, 2, 2, 2, 2, 2, 3)
+my_list = [1, 2, 3]
+
+# two methods: count() and index()
+print(my_tuple.count(2))
+print(my_tuple.index(3))
