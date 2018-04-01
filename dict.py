e3256342191627a5a7ff918f5e0293f5b3bd476e
Luca Terrazzan <luca.terraz@gmail.com>
2018-04-01 16:26:30 +0200

    Added dict examples

---
+++ dict.py
@@ -0,0 +1,17 @@
+# Some dictionaries examples
+
+my_dict = {
+    'k1': 2,
+    'k2': 5,
+    'list': ['a', 'b', 'c']
+}
+print(my_dict['k1'])
+print(my_dict['list'][2])
+
+my_dict['new'] = 22
+print(my_dict)
+
+# keys() and values()
+print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.items())
