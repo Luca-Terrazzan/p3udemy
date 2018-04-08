fbc7cd077b7fb484e45b6180b02dde20a5e146a2
Luca Terrazzan <luca.terraz@gmail.com>
2018-04-08 10:42:25 +0200

    Added account class for assignment

---
+++ src/oop/Account.py
@@ -0,0 +1,20 @@
+class Account():
+    """Account class
+    Used in python3 course assignment
+    """
+    def __init__(self, owner: str, balance: float = 0.00):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount: float):
+        if amount <= 0.00:
+            raise Exception('Deposits must be made on positive amounts!')
+        self.balance += amount
+
+    def withdraw(self, amount: float):
+        if amount < 0.00 or (self.balance - amount) < 0.00:
+            raise Exception('Deposits must be made on positive amounts!')
+        self.balance -= amount
+
+    def __str__(self):
+        return f'Account owner: {self.owner}. Accoutn balance: {self.balance}'
