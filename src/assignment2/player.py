025dfdc6b6e8aa99174ea2f1e3e62bcddbf92836
Luca Terrazzan <luca.terraz@gmail.com>
2018-04-08 22:58:17 +0200

    Player class and test

--- src/assignment2/player.py
+++ src/assignment2/player.py
@@ -10,20 +10,29 @@ class Player():
             bankroll {int} -- Player's starting bankroll (default: {100})
         """
         self.bankroll = bankroll
+        self.current_gamble = None
 
-    def gamble_amount(self, amount: int):
-        if amount > self.bankroll:
+    def play(self, bet: int):
+        if self.current_gamble is not None:
+            raise InvalidGamble("Hand not finished!")
+        if bet < 1:
+            raise InvalidGamble("Bet must be positive!")
+        if bet > self.bankroll:
             raise BankruptException("Cannot gamble that much!")
-        self.bankroll -= amount
-        self.current_gamble = amount
+        self.bankroll -= bet
+        self.current_gamble = bet
 
     def win_hand(self):
+        if self.current_gamble is None:
+            raise InvalidGamble("Cannot play a hand without having bet!")
         self.bankroll += self.current_gamble * 2
+        self.current_gamble = None
 
     def lose_hand(self):
-        self.bankroll -= self.current_gamble * 2
-        if self.bankroll <= 0:
-            raise BankruptException("Game lost!")
+        self.current_gamble = None
 
 class BankruptException(Exception):
+    pass
+
+class InvalidGamble(Exception):
     pass
---
+++ src/assignment2/player_test.py
@@ -0,0 +1,28 @@
+import unittest
+from player import *
+
+class PlayerTest(unittest.TestCase):
+    def test_player_wins_hand(self):
+        p = Player(50)
+        p.play(1)
+        p.win_hand()
+        self.assertEqual(p.bankroll, 51)
+        p.play(5)
+        p.win_hand()
+        self.assertEqual(p.bankroll, 56)
+
+    def test_player_loses_hand(self):
+        p = Player(50)
+        p.play(1)
+        p.lose_hand()
+        self.assertEqual(p.bankroll, 49)
+        p.play(5)
+        p.lose_hand()
+        self.assertEqual(p.bankroll, 44)
+
+    def test_player_bankrupts(self):
+        p = Player(2)
+        p.play(2)
+        p.lose_hand()
+        with self.assertRaises(BankruptException):
+            p.play(1)
