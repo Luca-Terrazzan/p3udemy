0e45252ad4eb4c3ea785cf2b1c9375d7b66c6f6f
Luca Terrazzan <luca.terraz@gmail.com>
2018-04-08 21:34:26 +0200

    updated player class

--- src/assignment2/player.py
+++ src/assignment2/player.py
@@ -1,9 +1,29 @@
 class Player():
     """Represents the human player
     """
+    bankroll: int
+    current_gamble: int
+
     def __init__(self, bankroll: int = 100):
         """Creates a player
         Keyword Arguments:
             bankroll {int} -- Player's starting bankroll (default: {100})
         """
         self.bankroll = bankroll
+
+    def gamble_amount(self, amount: int):
+        if amount > self.bankroll:
+            raise BankruptException("Cannot gamble that much!")
+        self.bankroll -= amount
+        self.current_gamble = amount
+
+    def win_hand(self):
+        self.bankroll += self.current_gamble * 2
+
+    def lose_hand(self):
+        self.bankroll -= self.current_gamble * 2
+        if self.bankroll <= 0:
+            raise BankruptException("Game lost!")
+
+class BankruptException(Exception):
+    pass
