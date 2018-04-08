0e1b06addef69272873e757acdbcc72c2137e191
Luca Terrazzan <luca.terraz@gmail.com>
2018-04-08 12:04:04 +0200

    linting

--- src/oop/oop_challenge/Account.py
+++ src/oop/oop_challenge/Account.py
@@ -1,17 +1,27 @@
+"""Account Class
+Raises:
+    Exception -- [description]
+    Exception -- [description]
+Returns:
+    [type] -- [description]
+"""
 class Account():
-    """Account class
-    Used in python3 course assignment
+    """Some account class
     """
     def __init__(self, owner: str, balance: float = 0.00):
         self.owner = owner
         self.balance = balance
 
     def deposit(self, amount: float):
+        """Deposit a sum
+        """
         if amount <= 0.00:
             raise Exception('Deposits must be made on positive amounts!')
         self.balance += amount
 
     def withdraw(self, amount: float):
+        """Withdraw a sum
+        """
         if amount < 0.00 or (self.balance < amount):
             raise Exception('Cannot withdraw the desired amount!')
         self.balance -= amount
