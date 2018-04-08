3fa45a131e14a82bb2ee0fa95150702fc6a33dfc
Luca Terrazzan <luca.terraz@gmail.com>
2018-04-08 23:01:56 +0200

    docs

--- src/assignment2/player.py
+++ src/assignment2/player.py
@@ -13,6 +13,14 @@ class Player():
         self.current_gamble = None
 
     def play(self, bet: int):
+        """Plays a hand
+        Arguments:
+            bet {int} -- The amount to bet
+        Raises:
+            InvalidGamble -- If another hand is currently in play OR
+                             an invalid amount has been passed
+            BankruptException -- If a game is started with an empty bankroll
+        """
         if self.current_gamble is not None:
             raise InvalidGamble("Hand not finished!")
         if bet < 1:
@@ -23,16 +31,26 @@ class Player():
         self.current_gamble = bet
 
     def win_hand(self):
+        """Wins a hand
+        Raises:
+            InvalidGamble -- If no hands are currently in play
+        """
         if self.current_gamble is None:
             raise InvalidGamble("Cannot play a hand without having bet!")
         self.bankroll += self.current_gamble * 2
         self.current_gamble = None
 
     def lose_hand(self):
+        """Loses a hand
+        """
         self.current_gamble = None
 
 class BankruptException(Exception):
+    """Raised when player acts with an empty bankroll
+    """
     pass
 
 class InvalidGamble(Exception):
+    """Raised when an invalid gamble is performed
+    """
     pass
